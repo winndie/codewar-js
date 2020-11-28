ef24616c79660be788701df0f00dc833c68ce9b6
Winndie Fan <winndie@gmail.com>
2020-11-28 12:28:29 +0000

    exercise002 is done.

--- challenges/exercise002.js
+++ challenges/exercise002.js
@@ -1,26 +1,42 @@
 function getFillings(sandwich) {
   if (sandwich === undefined) throw new Error("ingredients is required");
   // Your code here!
+  return sandwich.fillings;
 }
 
 function isFromManchester(person) {
   if (person === undefined) throw new Error("person is required");
   // Your code here!
+  return (person.city == "Manchester");
 }
 
 function getBusNumbers(people) {
   if (people === undefined) throw new Error("people is required");
   // Your code here!
+  return Math.ceil( people / 40 );
 }
 
 function countSheep(arr) {
   if (arr === undefined) throw new Error("arr is required");
   // Your code here!
+
+  var count = 0;
+
+  for (var i = 0; i < arr.length ; i++){
+
+    if (arr[i] == "sheep")
+      count++;
+
+  }
+
+  return count;
 }
 
 function hasMPostCode(person) {
   if (person === undefined) throw new Error("person is required");
   // Your code here!
+
+  return ( person.address.postCode.charAt(0) == "M" && person.address.city == "Manchester");
 }
 
 module.exports = {

8ceeb45e25474a3091a2dab2c71c43bf60602294
Winndie Fan <winndie@gmail.com>
2020-12-01 13:39:31 +0000

    tidy up exercise002

--- challenges/exercise002.js
+++ challenges/exercise002.js
@@ -1,24 +1,23 @@
 function getFillings(sandwich) {
   if (sandwich === undefined) throw new Error("ingredients is required");
-  // Your code here!
+
   return sandwich.fillings;
 }
 
 function isFromManchester(person) {
   if (person === undefined) throw new Error("person is required");
-  // Your code here!
+
   return (person.city == "Manchester");
 }
 
 function getBusNumbers(people) {
   if (people === undefined) throw new Error("people is required");
-  // Your code here!
+
   return Math.ceil( people / 40 );
 }
 
 function countSheep(arr) {
   if (arr === undefined) throw new Error("arr is required");
-  // Your code here!
 
   return arr
           .reduce( (acc, val) => { 
@@ -33,9 +32,8 @@ function countSheep(arr) {
 
 function hasMPostCode(person) {
   if (person === undefined) throw new Error("person is required");
-  // Your code here!
 
-  return ( person.address.postCode.charAt(0) == "M" && person.address.city == "Manchester");
+  return (person.address.postCode.charAt(0) == "M" && person.address.city == "Manchester");
 }
 
 module.exports = {
